032bfde472932ef0704a982f81e1ae93cd10f183
Andrianina <rbrstart@gmail.com>
2018-07-26 15:33:45 +0300

    angular-material 5.2.4 added to the project, it was funny

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MaterialModule } from './material.module';
 import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
@@ -31,13 +32,15 @@ import { GenerateDatePipe } from './pipes/generate.date.pipe';
     ProduitNewComponent,
     ProduitDetailComponent,
     ProduitEditComponent,
-    GenerateDatePipe
+    GenerateDatePipe,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    MaterialModule,
+    routing,
+    BrowserAnimationsModule
   ],
   providers: [
   	appRoutingProviders
---
+++ angular/src/app/material.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatInputModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatSelectModule,
+    MatInputModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule
+  ]
+})
+export class MaterialModule {}
